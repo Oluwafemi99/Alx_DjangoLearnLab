8386a7391e705edae989a7639fce4303b2eebcda
oluwafemi <oluwafemidojumo@gmail.com>
2025-02-12 15:25:02 +0200

    crud operations

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -7,6 +7,7 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publication_year']
     list_filter = ['title', 'author']
+    search_fields = ['title', 'author']
 
 
 admin.site.register(Book, BookAdmin)
