4543e74dca62e7da73180f10d81b7b9a2652f8df
oluwafemi <oluwafemidojumo@gmail.com>
2025-02-23 23:38:18 +0200

    django, login, logout

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -44,3 +44,10 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
