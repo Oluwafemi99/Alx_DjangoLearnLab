b3a60ff2e06624bfce5a5f47178e3c0a83adaf60
oluwafemi <oluwafemidojumo@gmail.com>
2025-02-23 18:04:27 +0200

    django, views, url

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .models import Book
-from .models import Library
+from .models import Book, Library
 from django.views.generic.detail import DetailView
+from django.http import HttpResponse
+from django.template import loader
+
 # Create a function-based view
 
 
@@ -9,6 +11,11 @@ def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
     return render(request, 'relationship_app/list_books.html', context)
+
+
+def books(request):
+    template = loader.get_template('list_book.html')
+    return HttpResponse(template.render())
 
 # class based views
 
@@ -19,6 +26,6 @@ class LibraryDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        book = self.get_object()
-        context['average_rating'] = book.get_average_rating()
+        library = self.get_object()
+        context['average_rating'] = library.get_average_rating()
         return context
