95d8e2fbaa1ff5fabd9f55c62031c47f9c89a13f
oluwafemi <oluwafemidojumo@gmail.com>
2025-02-23 19:55:04 +0200

    django, login, logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.LibraryDetailView.as_view(), name='book_detail'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('signup/', SignUpView.as_view(template_name='registration/signup.html'), name='signup'),
 ]
