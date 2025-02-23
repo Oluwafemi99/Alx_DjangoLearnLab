cfbb9840ac4afd2f01bf1024aba2dd308709fcb5
oluwafemi <oluwafemidojumo@gmail.com>
2025-02-23 15:24:25 +0200

    django models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,10 @@
 from .models import Author, Book, Library, Librarian
 
-author = Author.objects.get(Book)
+author_id = 1
+books_by_author = Book.objects.filter(author__id=author_id)
+
+# Get all books in the library
 books = Library.objects.all()
-liberian = Library.objects.get(Library=Librarian)
+
+# Assuming you want to get a librarian by a specific library's ID
+librarian = Librarian.objects.get(library__id=1)
