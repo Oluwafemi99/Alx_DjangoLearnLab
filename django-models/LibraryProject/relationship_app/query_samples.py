6bf4876dead35d838d3eccdd99edd430007caf87
oluwafemi <oluwafemidojumo@gmail.com>
2025-02-23 15:40:15 +0200

    django models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,9 @@
 from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author_id = 1  # Replace 1 with the actual author ID
-books_by_author = Book.objects.filter(author__id=author_id)
+author_name = "example_author_name" 
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
 
 # List all books in a library
 library_name = "example_library_name"  # Replace with the actual library name
