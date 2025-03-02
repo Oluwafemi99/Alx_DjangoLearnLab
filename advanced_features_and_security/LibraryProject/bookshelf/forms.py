b82f539dc730882e4a74efee6e38996893ec2dde
oluwafemi <oluwafemidojumo@gmail.com>
2025-03-02 13:09:47 +0200

    security

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Book, CustomUser
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
+
+class CustomUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'first_name', 'last_name', 'date_of_birth', 'profile_photo']
