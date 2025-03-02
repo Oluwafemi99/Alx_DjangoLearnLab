5be9266fd1e1ef04600df27b951370dab2c653a8
oluwafemi <oluwafemidojumo@gmail.com>
2025-03-02 13:45:26 +0200

    security

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -143,3 +143,11 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 # Ensure cookies are sent over HTTPS only
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+# Redirect all non-HTTPS requests to HTTPS
+SECURE_SSL_REDIRECT = True
+
+# HTTP Strict Transport Security (HSTS)
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
