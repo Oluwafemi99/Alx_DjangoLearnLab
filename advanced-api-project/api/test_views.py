c66d85aa977ad78e9b543a6f6a2fbe237f1a1ea8
oluwafemi <oluwafemidojumo@gmail.com>
2025-03-15 11:10:35 +0200

    test

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -19,7 +19,7 @@ class BookAPITestCase(TestCase):
         self.book = Book.objects.create(**self.book_data)
 
     def test_create_book(self):
-        self.client.force_authenticate(user=self.user)
+        self.client.login(username='testuser', password='testpass')
         data = {
             'title': 'New Book',
             'author': self.author.id,
@@ -31,7 +31,7 @@ class BookAPITestCase(TestCase):
         self.assertEqual(Book.objects.latest('id').title, 'New Book')
 
     def test_update_book(self):
-        self.client.force_authenticate(user=self.user)
+        self.client.login(username='testuser', password='testpass')
         updated_data = {
             'title': 'Updated Test Book',
             'author': self.author.id,
@@ -43,7 +43,7 @@ class BookAPITestCase(TestCase):
         self.assertEqual(self.book.title, 'Updated Test Book')
 
     def test_delete_book(self):
-        self.client.force_authenticate(user=self.user)
+        self.client.login(username='testuser', password='testpass')
         response = self.client.delete(f'/api/books/{self.book.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
@@ -58,9 +58,7 @@ class BookAPITestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], self.book.title)
 
-# Ensure the client is not authenticated
     def test_unauthenticated_create(self):
-        self.client.force_authenticate(user=None)  
         data = {
             'title': 'New Book',
             'author': self.author.id,
@@ -69,9 +67,7 @@ class BookAPITestCase(TestCase):
         response = self.client.post(reverse('book-list'), data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
- # client is not authenticated
     def test_unauthenticated_update(self):
-        self.client.force_authenticate(user=None) 
         updated_data = {
             'title': 'Updated Test Book',
             'author': self.author.id,
@@ -80,8 +76,6 @@ class BookAPITestCase(TestCase):
         response = self.client.put(f'/api/books/{self.book.id}/', updated_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-# Ensure the client is not authenticated
     def test_unauthenticated_delete(self):
-        self.client.force_authenticate(user=None) 
         response = self.client.delete(f'/api/books/{self.book.id}/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
