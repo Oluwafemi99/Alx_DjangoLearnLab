b1a1e20bfaf8ce5fbb339e58d41cb9ff870f0607
oluwafemi <oluwafemidojumo@gmail.com>
2025-03-21 17:21:42 +0200

    django_blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,12 +2,16 @@ from django import forms
 from .models import Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']  # Only include these fields in the form
+        widgets = {
+            'tags': TagWidget(attrs={'placeholder': 'Add tags here'}),
+        }
 
     def save(self, commit=True, user=None):
         """
