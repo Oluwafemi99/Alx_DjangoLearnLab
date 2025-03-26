e6583f5d7097e2625fb0d25d7d59e006bf9a43a1
oluwafemi <oluwafemidojumo@gmail.com>
2025-03-26 21:42:44 +0200

    social-media

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-from .views import SignUpView
+from .views import SignUpView, ProfileView
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='register'),
-    path('login/', obtain_auth_token, name='auth-token')
+    path('login/', obtain_auth_token, name='auth-token'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,20 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import CustomUserSerializer
 from .models import CustomUser
-from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class SignUpView(generics.CreateAPIView):
     serializer_class = CustomUserSerializer
-
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = CustomUser.objects.get(username=request.data['username'])
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key})
 
 
 class ProfileView(generics.RetrieveUpdateAPIView):
